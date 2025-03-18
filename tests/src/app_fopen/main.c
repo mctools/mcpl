cbeb53a35fc61961b336b93ab790e3ef8092c390
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-03-18 11:54:45 +0100

    Rename tests and add license blurb

--- tests/src/app_fopen/main.c
+++ tests/src/app_fopen/main.c
@@ -1,8 +1,25 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  This file is part of MCPL (see https://mctools.github.io/mcpl/)           //
+//                                                                            //
+//  Copyright 2015-2025 MCPL developers.                                      //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//      http://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
 #include <stdio.h>
-//#  include <unistd.h>
-//#  include <limits.h>
-//#  include <errno.h>
-//#  include <sys/stat.h>
 
 int main(int argc, char** argv)
 {
@@ -22,7 +39,7 @@ int main(int argc, char** argv)
     printf("Calling fclose\n");
     fclose(fh);
   }
-  return 0;
+
   //Time to modify:
   {
     printf("Calling fopen r+b\n");
