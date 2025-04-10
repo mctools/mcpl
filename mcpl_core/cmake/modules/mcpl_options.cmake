8ed46fc0998379e9abe2e64f57cb0fb2a0eb05df
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-04-10 12:56:02 +0200

    Work on fixmes

--- mcpl_core/cmake/modules/mcpl_options.cmake
+++ mcpl_core/cmake/modules/mcpl_options.cmake
@@ -28,9 +28,9 @@ include( mcpl_utils )
 #in the add_deprecated_boolvar(..) call as well!
 
 enum_option( MCPL_ENABLE_ZLIB "Whether to enable zlib support" DEFAULT FETCH FETCH_NG USEPREINSTALLED )
-bool_option( MCPL_ENABLE_CFGAPP    "Whether to build and install the mcpl-config command" "ON" )
+bool_option( MCPL_ENABLE_CFGAPP "Whether to build and install the mcpl-config command" "ON" )
 bool_option( MCPL_ENABLE_CORE_TESTING "Enable the few CTests fully contained within the mcpl_core project." "OFF" )
-enum_option( MCPL_BUILD_STRICT     "Stricter build (primarily for testing). Can optionally select specific C standard." "OFF" "ON" "99" "11" "14" "17" "23" )
+enum_option( MCPL_BUILD_STRICT "Stricter build (primarily for testing). Can optionally select specific standard." "OFF" "ON" "99" "11" "14" "17" "23" )
 
 if ( MCPL_ENABLE_ZLIB STREQUAL "DEFAULT" )
   if ( WIN32 )
