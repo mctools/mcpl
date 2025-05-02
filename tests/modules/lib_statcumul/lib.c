da592a43ae5b94a04065ba80c0e504c31f929405
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-05-02 09:00:17 +0200

    statcumul: Add py statcumul test with focus on error testing

---
+++ tests/modules/lib_statcumul/lib.c
@@ -0,0 +1,62 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  This file is part of MCPL (see https://mctools.github.io/mcpl/)           //
+//                                                                            //
+//  Copyright 2015-2025 MCPL developers.                                      //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//      http://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#include "mcpltestmodutils.h"
+
+MCPLTEST_CTYPE_DICTIONARY
+{
+  return
+    "void mcpltest_createstatcumulfile("
+    "       const char *,const char *,double,const char *,unsigned);"
+    "void mcpltest_dump( const char * );"
+    ;
+}
+
+MCPLTEST_CTYPES void mcpltest_createstatcumulfile( const char * filename,
+                                                   const char * statcumulname,
+                                                   double statcumulvalue,
+                                                   const char * customcomment,
+                                                   unsigned nparticles )
+{
+  mcpl_outfile_t f = mcpl_create_outfile( filename );
+  if ( strcmp(statcumulname,"<NONE>") != 0 )
+    mcpl_hdr_add_statcumul(f,statcumulname,statcumulvalue);
+  if ( strcmp(customcomment,"<NONE>") != 0 )
+    mcpl_hdr_add_comment(f,customcomment);
+
+  if ( nparticles ) {
+    mcpl_particle_t * particle = mcpl_get_empty_particle(f);
+    particle->direction[0] = 0.0;
+    particle->direction[1] = 1.0;
+    particle->direction[2] = 0.0;
+    particle->ekin = 0.025;
+    particle->pdgcode = 212;
+    particle->weight = 0.123;
+    for( unsigned i = 0; i < nparticles; ++i )
+      mcpl_add_particle(f,particle);
+  }
+  mcpl_close_outfile(f);
+}
+
+MCPLTEST_CTYPES void mcpltest_dump( const char * filename )
+{
+  mcpl_dump(filename,0,0,0);
+}
---
+++ tests/scripts/statcumul.py
@@ -0,0 +1,68 @@
+
+################################################################################
+##                                                                            ##
+##  This file is part of MCPL (see https://mctools.github.io/mcpl/)           ##
+##                                                                            ##
+##  Copyright 2015-2025 MCPL developers.                                      ##
+##                                                                            ##
+##  Licensed under the Apache License, Version 2.0 (the "License");           ##
+##  you may not use this file except in compliance with the License.          ##
+##  You may obtain a copy of the License at                                   ##
+##                                                                            ##
+##      http://www.apache.org/licenses/LICENSE-2.0                            ##
+##                                                                            ##
+##  Unless required by applicable law or agreed to in writing, software       ##
+##  distributed under the License is distributed on an "AS IS" BASIS,         ##
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  ##
+##  See the License for the specific language governing permissions and       ##
+##  limitations under the License.                                            ##
+##                                                                            ##
+################################################################################
+
+#NB: Written carefully with __name__ protection we can call functions with
+#pythons multiprocessing
+
+from MCPLTestUtils.loadlib import getlib
+import pathlib
+lib = getlib('statcumul')
+
+def dump( filename ):
+    lib.mcpltest_dump( filename )
+
+def create( filename='f.mcpl', *,
+            statcumul = None,
+            comment = None,
+            nparticles = 0,
+            do_dump = True ):
+    p = pathlib.Path(filename)
+    if p.is_file():
+        p.unlink()
+    if statcumul:
+        sc_name,sc_val = statcumul
+    else:
+        sc_name,sc_val = '<NONE>',-99.0
+    if comment is None:
+        comment = '<NONE>'
+    assert 0 <= nparticles <= 4294967295
+    lib.mcpltest_createstatcumulfile( filename,
+                                      sc_name,
+                                      sc_val,
+                                      comment,
+                                      int(nparticles) )
+    assert p.is_file()
+    if do_dump:
+        dump(filename)
+
+def create_bad( *a, **kw ):
+    lib.run_fct_expected_to_fail(create,*a,**kw)
+
+def main():
+    lib.dump()
+    create()
+    create(statcumul=('a'*63,0.0))
+    create(statcumul=('a'*64,0.0))
+    create_bad(statcumul=('a'*65,0.0))
+    print("after fail")
+
+if __name__ == '__main__':
+    main()
