84122215e5abb4dd0ad0c9b0a068303f544ebddc
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2025-05-22 14:41:13 +0200

    Unit test new MPI utilities

---
+++ tests/src/app_writempi/main.c
@@ -0,0 +1,91 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  This file is part of MCPL (see https://mctools.github.io/mcpl/)           //
+//                                                                            //
+//  Copyright 2015-2025 MCPL developers.                                      //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//      http://www.apache.org/licenses/LICENSE-2.0                            //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+#include "mcpl.h"
+#include <stdio.h>
+#include <memory.h>
+
+int main(int argc,char**argv) {
+  (void)argc;
+  (void)argv;
+
+  //Pretend MPI (but actually run in one process:
+
+#define MCPLTEST_NPROC 4
+  const unsigned nproc = MCPLTEST_NPROC;
+
+  //mpi: init files:
+  //init all mpi procs:
+  mcpl_outfile_t outhandles[MCPLTEST_NPROC];
+  for ( unsigned iproc = 0; iproc < nproc; ++iproc ) {
+
+    //It makes no difference if the filename argument is "foobar", "foobar.mcpl"
+    //or "foobar.mcpl.gz". Unit test this:
+    const char * userfn;
+    if ( iproc % 3 == 0 )
+      userfn = "foobar";
+    else if ( iproc % 3 == 1 )
+      userfn = "foobar.mcpl";
+    else
+      userfn = "foobar.mcpl.gz";
+
+    mcpl_outfile_t f = mcpl_create_outfile_mpi( userfn, iproc, nproc );
+    outhandles[iproc] = f;
+    mcpl_hdr_set_srcname(f,"CustomMPITest");
+    mcpl_enable_universal_pdgcode(f,2112);
+    mcpl_hdr_add_comment(f,"Some comment.");
+    mcpl_hdr_add_comment(f,"Another comment.");
+    mcpl_hdr_add_stat_sum(f,"foostat", -1.0 );
+  }
+
+  //mpi: add particles:
+  for ( unsigned iproc = 0; iproc < nproc; ++iproc ) {
+    mcpl_outfile_t f = outhandles[iproc];
+    mcpl_particle_t * particle = mcpl_get_empty_particle(f);
+    for (int i = 0; i < 2; ++i) {
+      particle->position[0] = i*1.0;
+      particle->position[1] = i*2.0;
+      particle->position[2] = i*3.0;
+      particle->ekin = i*0.1;
+      particle->direction[0] = 0.0;
+      particle->direction[1] = 0.0;
+      particle->direction[2] = 1.0;
+      particle->time = i*0.01;
+      particle->weight = (double)iproc;
+      mcpl_add_particle(f,particle);
+    }
+  }
+
+  //mpi: close files
+  for ( unsigned iproc = 0; iproc < nproc; ++iproc ) {
+    mcpl_outfile_t f = outhandles[iproc];
+    mcpl_hdr_add_stat_sum(f,"foostat", 10.0 + iproc );
+    mcpl_closeandgzip_outfile(f);
+  }
+
+  //Merge:
+  mcpl_merge_outfiles_mpi( "foobar", nproc );
+
+  //Dump:
+  mcpl_dump("foobar.mcpl.gz", 0, 0, 0);
+
+  return 0;
+}
