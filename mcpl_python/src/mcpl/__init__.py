08fad5d6053886d755ea9d30856c31a832595496
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-01-06 17:03:55 +0100

    Update version numbers to 2.2.6

--- mcpl_python/src/mcpl/__init__.py
+++ mcpl_python/src/mcpl/__init__.py
@@ -60,7 +60,7 @@ __all__ = [ 'MCPLFile',
             'is_valid_stat_sum_key'
            ]
 
-__version__ = '2.2.5'
+__version__ = '2.2.6'
 
 from .mcpl import ( MCPLFile,
                     MCPLParticle,
