1d1e3fdbf04a92ca93862f5065e032cf962f58c4
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2026-02-18 11:00:06 +0100

    Update version numbers to 2.2.8

--- mcpl_python/src/mcpl/__init__.py
+++ mcpl_python/src/mcpl/__init__.py
@@ -60,7 +60,7 @@ __all__ = [ 'MCPLFile',
             'is_valid_stat_sum_key'
            ]
 
-__version__ = '2.2.7'
+__version__ = '2.2.8'
 
 from .mcpl import ( MCPLFile,
                     MCPLParticle,
